1b4ed32384bc59527ac046a43dc18a1b1414c9f0
anvitha <21jr1a05g0@gmail.com>
2025-09-15 23:47:46 +0530

    single

---
+++ single.py
@@ -0,0 +1,10 @@
+class Report:
+    def generate(self):
+        print("generating report")
+class save:
+    def saving(self):
+        print("report saved")
+r=Report()
+r.generate()
+s=save()
+s.saving()
